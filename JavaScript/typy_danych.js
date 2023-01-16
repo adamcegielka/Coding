36b4f300bc9b162baf880b4386b0300e03f79cb4
Adam <adamcegielka@tlen.pl>
2023-01-16 15:24:58 +0100

    Adding file typy_danych.js

---
+++ JavaScript/typy_danych.js
@@ -0,0 +1,31 @@
+console.log("Hello World!");
+console.log("Hello. How are you today?");
+
+var variableName = "Great and you?"; // zmienna: var
+console.log(variableName);
+
+let textA = "What will we learn today?"; // zmienna: let
+console.log("Great too.", textA);
+
+let textB = 10;
+console.log("Maybe", textB, "new 'words' in English");
+
+console.log("OK \nGood luck!"); // \n -wyświetla tekst z nowego wiersza
+
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nunc nec malesuada libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                Ut sed risus sed lectus efficitur sollicitudin eget ac eros.`;
+console.log(longText);
+
+// tworzenie tablic z wieloma elementami
+let tab = [5, "Jonh Smith", 42, 68];
+console.log("Second element of the table: " + tab[1]);
+console.log("Third element of the table: " + tab[3]);
+
+alert("Funkcje wyświetlone są w konsoli"); // wyskakujący komunikat
+
+// Komentarz w JavaScript jednoliniowy
+
+/*
+      Komentarz w JavaScript wieloliniowy
+      */
