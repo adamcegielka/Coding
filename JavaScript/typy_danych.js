46ce98495caed3d9c5d7e7f4448304fb5cde9a0c
Adam <adamcegielka@tlen.pl>
2023-01-16 13:30:08 +0100

    Change in the JavaScript catalog

---
+++ JavaScript/typy_danych.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+      console.log("Hello. How are you today?");
+
+      var variableName = "Great and you?"; // zmienna: var
+      console.log(variableName);
+
+      let textA = "What will we learn today?"; // zmienna: let
+      console.log("Great too.", textA);
+
+      let textB = 10;
+      console.log("Maybe", textB, "new 'words' in English");
+
+      console.log("OK \nGood luck!"); // \n -wyświetla teks z nowego wiersza
+
+      alert("Funkcje wyświetlone są w konsoli"); // wyskakujący komunikat
+
+      // Komentarz w JavaScript jednolinowy
+
+      /*
+      Komentarz w JavaScript wieloliniowy
+      */
+    </script>
+    <!-- document.write("Hello World") - wyświetla napis przez konsole na stronie -->
