1a5ffdadbbd49f713a16b722a68c4b73935d011e
Adam <adamcegielka@tlen.pl>
2023-04-14 15:28:39 +0200

    Adding Java Lesson

---
+++ Java-udemy course/src/Lesson01HelloAdam.java
@@ -0,0 +1,12 @@
+public class Lesson01HelloAdam {
+    // metoda main - glowna metoda, w kazdej aplikacji ktora jest wykonywana podczas uruchomienia aplikacji
+    public static void main(String[] args) {
+        // wpisuje na ekranie konkretny ciag znakow
+        System.out.println("Hello Adam");
+        System.out.println("I am fine");
+
+        String wishes = "Here's my best wishes to you on";
+        System.out.println(wishes + " " +
+                "International Women's Day 2023!");
+    }
+}
