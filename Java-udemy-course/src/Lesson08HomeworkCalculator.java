4dab756b0739f9d255bd1ab730c083cb13a07a32
Adam <adamcegielka@tlen.pl>
2023-04-15 17:58:57 +0200

    Adding Lesson08HomeworkCalculator in lessons Java-udemy

---
+++ Java-udemy-course/src/Lesson08HomeworkCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Lesson08HomeworkCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a first  number:");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter a second  number:");
+        int secondNumber = scanner.nextInt();
+
+        int addition = firstNumber + secondNumber;
+        int subraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int division = firstNumber / secondNumber;
+        int mod = secondNumber % firstNumber;
+
+        System.out.println("The result of addition is: " + addition);
+        System.out.println("The result of subraction is:: " + subraction);
+        System.out.println("The result of multiplication is:: " + multiplication);
+        System.out.println("The result of division is:: " + division);
+        System.out.println("The result of mod is:: " + mod);
+    }
+}
