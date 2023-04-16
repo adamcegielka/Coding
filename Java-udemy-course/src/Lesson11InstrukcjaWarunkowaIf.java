e8fb99aa3af45a587f1682a38b6c125546b76ef1
Adam <adamcegielka@tlen.pl>
2023-04-16 15:45:39 +0200

    Adding Lesson11InstrukcjaWarunkowaIf in lessons Java-udemy

---
+++ Java-udemy-course/src/Lesson11InstrukcjaWarunkowaIf.java
@@ -0,0 +1,13 @@
+public class Lesson11InstrukcjaWarunkowaIf {
+
+    public static void main(String[] args) {
+        float firstNumber = 8.0F;
+        float secondNumber = 5.0F;
+
+        if(secondNumber != 0) {
+            System.out.println("Wynik dzielenia: " + (firstNumber / secondNumber));
+        } else {
+            System.out.println("Podaj inną liczbę");    // Przy wpisaniu liczby 0 w secondNumber
+        }
+    }
+}
