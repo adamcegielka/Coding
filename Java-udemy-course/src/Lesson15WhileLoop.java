b8cea4c6a5c79d143975d235c062012ef5f8a704
Adam <adamcegielka@tlen.pl>
2023-04-18 17:41:57 +0200

    Lesson15WhileLoop

---
+++ Java-udemy-course/src/Lesson15WhileLoop.java
@@ -0,0 +1,11 @@
+public class Lesson15WhileLoop {
+    public static void main(String[] args) {
+
+        int number = 11;
+
+        while (number < 20) {
+            System.out.println("My value is: " + number + " and I am less than 20");
+            number++;
+        }
+    }
+}
