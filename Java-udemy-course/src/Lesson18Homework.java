5bcf28336aee5f477dbd2727927f8056d1046774
Adam <adamcegielka@tlen.pl>
2023-04-19 12:15:35 +0200

    🛠️ Editing Lesson18Homework in a Java-udemy-course

--- Java-udemy-course/src/Lesson18Homework.java
+++ Java-udemy-course/src/Lesson18Homework.java
@@ -32,7 +32,7 @@ public class Lesson18Homework {
         int[] numbers = new int[] {1, 3, 5, 7, 0};
 
         for (int i = 0; i < (numbers.length/2); i++) {
-            int temp = numbers[1];                          // dla i = 0  1  // i = 1  3
+            int temp = numbers[i];                          // dla i = 0  1  // i = 1  3
             numbers[i] = numbers[numbers.length - 1 - i];   // 4-> 0         // 3 => 7
             numbers[numbers.length - 1 - i] = temp;         // 1             // mumbers[3] = 3
         }
