921a62d2e2f652695873bf87d60a37eb8a7e9150
Adam <adamcegielka@tlen.pl>
2023-04-19 16:45:13 +0200

    🛠️ Editing 01_typy_danych

--- JavaScript-YouTube-course/01_typy_danych.js
+++ JavaScript-YouTube-course/01_typy_danych.js
@@ -4,26 +4,38 @@ console.log("Hello. How are you today?");
 var variableName = "Great and you?"; // zmienna: var
 console.log(variableName);
 
+var x = 6;
+console.log(x + x);
+
 let textA = "What will we learn today?"; // zmienna: let
 console.log("Great too.", textA);
 
 let textB = 10;
 console.log("Maybe", textB, "new 'words' in English");
 
-console.log("OK \nGood luck!"); // \n -wyświetla tekst z nowego wiersza
+const a = 22;
+console.log(a);
 
+
+// \n -wyświetla tekst z nowego wiersza
+console.log("OK \nGood luck!"); 
+
+
+// znak `Jakiś tekst` pozwala na wpsanie długigo tekstu w wielu liniach
 let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Nunc nec malesuada libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                 Ut sed risus sed lectus efficitur sollicitudin eget ac eros.`;
+console.log(longText); 
 
-console.log(longText); // znak `Jakis teks` pozwala na wpsanie długigo tekstu w wielu liniach
 
 // tworzenie tablic z wieloma elementami
 let tab = [5, "Jonh Smith", 42, 68];
 console.log("Second element of the table: " + tab[1]);
 console.log("Third element of the table: " + tab[3]);
 
-alert("Funkcje wyświetlone są w konsoli"); // wyskakujący komunikat
+
+// wyskakujący komunikat
+alert("Funkcje wyświetlone są w konsoli"); 
 
 
 // Komentarz w JavaScript jednoliniowy
