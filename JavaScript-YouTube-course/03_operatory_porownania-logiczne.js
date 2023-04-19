edf3e1d127ca36af4658613c729e5274cb8f1d0c
Adam <adamcegielka@tlen.pl>
2023-04-19 17:37:32 +0200

    🔧 Editing 03_operatory_porownania_logiczne

--- JavaScript-YouTube-course/03_operatory_porownania-logiczne.js
+++ JavaScript-YouTube-course/03_operatory_porownania-logiczne.js
@@ -3,38 +3,40 @@
 console.log("Operatory Porównania i Logiczne");
 
 let x = 4;
-console.log(typeof x); // Określa rodzaj zmiennej x=number
+console.log(x);
+console.log(typeof (x)); // Określa rodzaj zmiennej "number"
+
+console.log("-----------------")
 
 // OPERATORY PORÓWNANIA:
-
 let numberA = 8;
 let numberB = 6;
 let numberC = 8;
 let numberD = "8";
 
-console.log(numberA == numberC); // true
-console.log(numberA == numberD); // true
-console.log(numberA === numberD); // false - sprawdza dodatkowo czy typ jest taki sam
-console.log(numberA != numberB); // true
-console.log(numberA != numberD); // false
-console.log(numberA !== numberD); // true
-console.log(numberA >= numberB); // true
-console.log(numberA <= numberB); // false
-console.log(numberA > numberB); // true
-console.log(numberA < numberB); // false
+console.log(numberA == numberC);    // true
+console.log(numberA == numberD);    // true
+console.log(numberA === numberD);   // false - sprawdza dodatkowo czy typ jest taki sam
+console.log(numberA != numberB);    // true
+console.log(numberA != numberD);    // false
+console.log(numberA !== numberD);   // true
+console.log(numberA >= numberB);    // true
+console.log(numberA <= numberB);    // false
+console.log(numberA > numberB);     // true
+console.log(numberA < numberB);     // false
 
 // OPERATORY LOGICZNE:
 
 let numberE = true;
 let numberG = false;
 
-console.log(numberE || numberG); // "OR" -  true || false = true
-console.log(numberE || numberE); // "OR" -  true || true = true
-console.log(numberG || numberG); // "OR" -  true || false = false
+console.log(numberE || numberG);  // "OR" -  true || false = true
+console.log(numberE || numberE);  // "OR" -  true || true = true
+console.log(numberG || numberG);  // "OR" -  true || false = false
 
-console.log(numberE && numberE); // "AND" -  true &&| true = true
-console.log(numberE && numberG); // "AND" -  true && false = false
-console.log(numberG && numberG); // "AND" -  false && false = false
+console.log(numberE && numberE);  // "AND" -  true &&| true = true
+console.log(numberE && numberG);  // "AND" -  true && false = false
+console.log(numberG && numberG);  // "AND" -  false && false = false
 
 console.log(!numberE == numberE); // "! Negacja" -  (false) && true = false
 console.log(!numberG == numberG); // "! Negacja" -  (true) && false = false
