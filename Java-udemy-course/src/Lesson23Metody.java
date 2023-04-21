41f8f0660646836e312630cd10d469f134698cbe
Adam <adamcegielka@tlen.pl>
2023-04-21 10:00:42 +0200

    📌 Adding Lesson23Metody in a Java-udemy-course

---
+++ Java-udemy-course/src/Lesson23Metody.java
@@ -0,0 +1,11 @@
+public class Lesson23Metody {
+
+     public void countingNumbers() {
+         System.out.println("Number coutning");
+         int result = 1;
+         for (int i = 1; i < 49; i++) {
+             result = result + i;
+         }
+         System.out.println("Restult is: " + result);
+     }
+}
---
+++ Java-udemy-course/src/Lesson23MetodyCountingNumbers.java
@@ -0,0 +1,6 @@
+public class Lesson23MetodyCountingNumbers {
+    public static void main(String[] args) {
+        Lesson23Metody lesson23Metody = new Lesson23Metody();
+        lesson23Metody.countingNumbers();
+    }
+}
