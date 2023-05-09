963074d0f4fc6442d09965cf5766947d4084ca41
Adam <adamcegielka@tlen.pl>
2023-05-09 16:14:12 +0200

    🚀 Additing task01

---
+++ JavaScript-My-Lerning/tasks/task01_CalculateAverage.js
@@ -0,0 +1,17 @@
+console.log("Calculating the average of numbers: 1, 3, 5, 7");
+
+let one = (5 - 4) / 2;
+console.log("Average of 1:" + " " + one);            // 0.5
+
+let there = (2 + 1) / 2;
+console.log("Average of 3:" + " " + there);          // 1.5
+
+let five = (15 / 3) / 2;
+console.log("Average of 5:" + " " + five);           // 2.5
+
+let seven = (2 * 2 + 3) / 2;
+console.log("Average of 7:" + " " + seven);          // 3.5
+
+
+let result = (one + there + five + seven) / 4;       // 2
+console.log(result);
