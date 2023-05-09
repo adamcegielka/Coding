eb909a7870cebd1835d389352d9f2df5a43bbeac
Adam <adamcegielka@tlen.pl>
2023-05-09 16:50:17 +0200

    🚀 Additing task00

---
+++ JavaScript-My-Lerning/tasks/task00_EmailsendingDetails.js
@@ -0,0 +1,23 @@
+console.log("Email sending details");
+
+
+let emailTitle = "Software Testing Course";
+console.log("Title:", emailTitle);
+
+let sender = "userSender@example.com";
+console.log("Sender:", sender);
+
+let recipient = "userRecipient@example.com";
+console.log("Recipient:", recipient);
+
+let emailContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+console.log("Content:", emailContent);
+
+let emailDate = "09,05,2023";
+console.log("Date:", emailDate);
+
+let emailAttachementPdf = "file.pdf";
+console.log("Attachement pdf:", emailAttachementPdf);
+
+let emailAttachementPng = "file.img";
+console.log("Attachement png:", emailAttachementPng);
