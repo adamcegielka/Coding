e4d529eb05db696743142e34d04c1f8a791d5c2e
Adam <adamcegielka@tlen.pl>
2023-05-09 16:42:02 +0200

    🚀 Additing task02

---
+++ JavaScript-My-Lerning/tasks/task02_GettingUserInformation.js
@@ -0,0 +1,8 @@
+console.log("Getting user information");
+
+let firsName = prompt("Insert name:");
+let lastName = prompt("Insert sorname:");
+let country = prompt("Insert country:");
+let age = prompt("How old are you:");
+
+console.log("Your name: " + firsName + " " + lastName + ", you are from " + country + ", and you are " + age + " years old");
