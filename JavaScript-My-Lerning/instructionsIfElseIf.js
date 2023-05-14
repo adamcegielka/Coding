97e5036b9371ad57be049635ac76e9386678de37
Adam <adamcegielka@tlen.pl>
2023-05-14 12:48:39 +0200

    🚀 Additing instructionsIfElseIf.js

---
+++ JavaScript-My-Lerning/instructionsIfElseIf.js
@@ -0,0 +1,53 @@
+// Conditional statements if, if else
+
+// Not return the text
+if (0) console.log("not return the text");
+if (false) console.log("not return the text");
+
+// Return the text
+if (-2) console.log("return the text");
+if (true) console.log("return the text");
+
+// new code
+let x = 5;
+let y = 9;
+
+if (x < y) {
+    console.log("It is: " + true);
+}
+
+// new code
+let z = 7;
+
+if (z >= 9) {
+    console.log("It is true")
+} else {
+    console.log("It is false")      // this cod will be executed
+}
+
+// new code
+let a = 17;
+if (a == 18) {
+    console.log("Ok");
+} else if (a > 18) {
+    console.log("OK");
+} else {
+    console.log("NO!");             // this cod will be executed
+}
+
+// new code
+let b = 5;
+
+if (b == 1) {
+    console.log("b = 1");
+} else if (b == 2) {
+    console.log("b = 2");
+} else if (b == 3) {
+    console.log("b = 3");
+} else if (b == 4) {
+    console.log("b = 4");
+} else if (b == 5) {
+    console.log("b = 5");
+} else {
+    console.log("does not meet the conditions");
+}
