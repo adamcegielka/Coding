84e4077be73af021f9d541686415ad0a4062cec4
Adam <adamcegielka@tlen.pl>
2023-05-14 13:22:01 +0200

    🔧 Update

--- JavaScript-My-Lerning/instructionsIfElseIf.js
+++ JavaScript-My-Lerning/instructionsIfElseIf.js
@@ -1,12 +1,16 @@
-// Conditional statements if, if else
+// Conditional statements if, else if, else
 
 // Not return the text
-if (0) console.log("not return the text");
+if (0) console.log("not return the text");      // 0 gives false
+if (5 - 5) console.log("not return the text");  // because the result gives 0
 if (false) console.log("not return the text");
+if (true != true) console.log("not return the text");
 
 // Return the text
-if (-2) console.log("return the text");
+if (-2) console.log("return the text");         // because it is not 0 and 0 gives false
+if (5 - 4) console.log("return the text");
 if (true) console.log("return the text");
+if (true == true) console.log("return the text");
 
 // new code
 let x = 5;
@@ -39,15 +43,15 @@ if (a == 18) {
 let b = 5;
 
 if (b == 1) {
-    console.log("b = 1");
+    console.log("b = ", b);
 } else if (b == 2) {
-    console.log("b = 2");
+    console.log("b = ", b);
 } else if (b == 3) {
-    console.log("b = 3");
+    console.log("b = ", b);
 } else if (b == 4) {
-    console.log("b = 4");
+    console.log("b = ", b);
 } else if (b == 5) {
-    console.log("b = 5");
+    console.log("b = ", b);           // this cod will be executed
 } else {
     console.log("does not meet the conditions");
 }
