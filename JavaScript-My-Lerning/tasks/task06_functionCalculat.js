1873dc1a5e7ad7a64928b94d8548a6047d930fb4
Adam <adamcegielka@tlen.pl>
2023-06-16 16:13:48 +0200

    🚀 Additing task06

---
+++ JavaScript-My-Lerning/tasks/task06_functionCalculat.js
@@ -0,0 +1,9 @@
+// Calculation of two values: weight and height
+
+function calculateBmi(weigh, height) {
+    let resultBmi = weigh / (height * height);
+    return resultBmi;
+}
+
+const bmi = calculateBmi(81, 1.73);
+console.log("Bmi value: " + bmi);       // 27.064051588760066
