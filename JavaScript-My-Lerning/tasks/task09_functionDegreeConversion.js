ff160bf396114f1fb335ffe0ee803c8a547fe291
Adam <adamcegielka@tlen.pl>
2023-06-16 17:20:04 +0200

    🚀 Additing task09

---
+++ JavaScript-My-Lerning/tasks/task09_functionDegreeConversion.js
@@ -0,0 +1,16 @@
+// Conversion of degrees celsius and fahrenheit
+
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = celsius * 9 / 5 + 32;
+    console.log(celsius + "\xB0 Celsius is " + fahrenheit + "\xB0 Fahrenheit ");
+    return fahrenheit;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    console.log(fahrenheit + "\xB0 Fahrenheit " + celsius + "\xB0 Celsius");
+    return celsius;
+}
+
+let f = celsiusToFahrenheit(20);
+let c = fahrenheitToCelsius(86);
