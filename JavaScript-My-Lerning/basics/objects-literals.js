5fb259fd4245e3a59243038b3336157cd6a68b60
Adam <adamcegielka@tlen.pl>
2023-06-17 12:00:15 +0200

    🚀 Additing objects-literals.js

---
+++ JavaScript-My-Lerning/basics/objects-literals.js
@@ -0,0 +1,21 @@
+// Objects Literals:
+
+let car = {
+    brand: "Audi",
+    model: "Q7",
+    color: "Red",
+    year: 2022,
+    price: 400000
+}
+car.price = 350000;
+console.log(car);
+
+// -------------------------------------------------
+
+let email = {
+    name: "Jan Nowak",
+    emailAddress: "jannowak@example.con",
+    title: "What is Lorem Ipsum?",
+    message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+}
+console.log(email);
