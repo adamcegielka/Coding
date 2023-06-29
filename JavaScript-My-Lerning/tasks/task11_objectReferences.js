3828994e676745d6d35baadbcf3ccd1ed2959266
Adam <adamcegielka@tlen.pl>
2023-06-29 12:35:14 +0200

    feat: add new file with task11

---
+++ JavaScript-My-Lerning/tasks/task11_objectReferences.js
@@ -0,0 +1,16 @@
+// Object references
+
+let car1 = {
+    brand: "Mercedes",
+    name: "EQE SUV",
+    year: 2022
+};
+
+let car2 = car1;
+car2 = car2.brand;  // 'Mercedes'
+console.log(car2.name); // 'EQE SUV'
+
+// adding to the object:
+
+car2.topSpeed = 290;
+console.log(car2);  // {brand: 'Mercedes', name: 'EQE SUV', year: 2022, topSpeed: 290}
