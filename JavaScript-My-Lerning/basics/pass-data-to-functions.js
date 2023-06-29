69daac61e53a285c3ef9827c98ad5d2c2af3673d
Adam <adamcegielka@tlen.pl>
2023-06-29 13:05:58 +0200

    feat: add new file pass-data-to-functions.js

---
+++ JavaScript-My-Lerning/basics/pass-data-to-functions.js
@@ -0,0 +1,11 @@
+// pass data to functions
+
+let a = 4;
+let b = "books";
+function passByValue(num, str) {
+    num = num + 5;
+    str = str + " !";
+    console.log("message: " + num + " " + str);
+}
+
+passByValue(a, b);
