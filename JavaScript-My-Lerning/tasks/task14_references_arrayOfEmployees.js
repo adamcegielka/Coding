cd513f761f00e624b48276e8ba415a2248fcbe96
Adam <adamcegielka@tlen.pl>
2023-07-04 17:15:40 +0200

    feat: add new file with task14

---
+++ JavaScript-My-Lerning/tasks/task14_references_arrayOfEmployees.js
@@ -0,0 +1,23 @@
+let company = {
+    name: "Example Ltd",
+    street: "Long",
+    city: "London",
+    country: "UK",
+    employees: []
+};
+
+function addEmpliyee(name, email, job) {
+    let employee = {
+        userName: name,
+        email: email,
+        job: job
+    };
+
+    company.employees.push(employee);
+};
+
+addEmpliyee("John", "john@example.com", "QA");
+addEmpliyee("Anna", "anna@example.com", "HR");
+addEmpliyee("Ewa", "ewa@example.com", "Manager");
+
+console.log(company);
