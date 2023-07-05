93367381ad12fc7442e0b4bc9d5c3db3235ef075
Adam <adamcegielka@tlen.pl>
2023-07-05 17:08:06 +0200

    feat: add new file with task15

---
+++ JavaScript-My-Lerning/tasks/tesk15_typeof.js
@@ -0,0 +1,21 @@
+function createUser(name, contact) {
+    let user = {
+        name: name,
+        email: null,
+        phone: null
+    };
+
+    if (typeof contact == 'string') {
+        user.email = contact;
+    } else if (typeof contact == 'number') {
+        user.phone = contact;
+    }
+
+    return user;
+};
+
+let user1 = createUser('Adam', 'adam@example.com');
+console.log(user1);
+
+let user2 = createUser('Ewa', 123456789);
+console.log(user2);
