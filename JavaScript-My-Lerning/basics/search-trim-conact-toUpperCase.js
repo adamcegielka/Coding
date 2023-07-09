8f69c0c234e7d92a66c8041c9912f962935ed1c3
Adam <adamcegielka@tlen.pl>
2023-07-09 18:27:36 +0200

    feat: add `search-trim-conact-toUpperCase.js` file

---
+++ JavaScript-My-Lerning/basics/search-trim-conact-toUpperCase.js
@@ -0,0 +1,70 @@
+// search()
+let regularExpression = /[A-Z]/g;
+let message = 'some Tekst';
+console.log(message.search(regularExpression));       // 5
+
+// --------------------------------------
+
+// trim() - deletes white marks
+let text0 = '   Hello World! \n  ';
+console.log(text0.trim());
+
+// --------------------------------------
+
+// toUpperCase(), toLowerCase()
+let text1 = 'Some Text'
+console.log(text1.toUpperCase());       // SOME TEXT
+
+let text2 = 'Some Text'
+console.log(text2.toLowerCase());       // some text
+
+// --------------------------------------
+
+// concat()
+let x = 'something1';
+let y = x.concat(' something2');
+console.log(x);     // something1
+console.log(y);     // something1 something2
+
+let z = y.concat(' something3').concat(' something4');
+console.log(z);     // something1 something2 something3 something4
+
+let q = z
+console.log(q.concat(' something5').concat(' something6'));
+    // something1 something2 something3 something4 something5 something6
+
+// --------------------------------------
+
+function gerUserInformation(name, surname, job) {
+    name = name.toLowerCase().trim();
+    surname = surname.toUpperCase().trim();
+    job = job.trim();
+
+    let newUser = `
+    Name: ${name}
+    Surname: ${surname}
+    Job: ${job}
+    `;
+    newUser = newUser.concat(`All: ${name} ${surname} - ${job}`);
+
+    return newUser;
+};
+
+let newUser1 = gerUserInformation('Jhon', 'Smith', 'Tester');
+console.log(newUser1);
+
+console.log(gerUserInformation('Eve', 'Fox', 'HR'));
+
+/*
+    Name: jhon
+    Surname: SMITH
+    Job: Tester
+    All: jhon SMITH - Tester
+*/
+
+/*
+    Name: eve
+    Surname: FOX
+    Job: HR
+    All: eve FOX - HR
+*/
