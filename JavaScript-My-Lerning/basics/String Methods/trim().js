ceee2bdb5c649cc431d5a472e1038e10805c3d80
Adam <adamcegielka@tlen.pl>
2023-07-10 17:05:24 +0200

    feat: add `trim().js` file

---
+++ JavaScript-My-Lerning/basics/String Methods/trim().js
@@ -0,0 +1,16 @@
+// String trim() - deletes white marks
+
+let text1 = '   Hello World! \n   ';
+console.log(text1.trim());      // Hello World!
+
+// --------------------------------------
+
+function removeUnnecessarySpaces(text) {
+    let trimmedText = text.trim();
+    console.log("Trimmed Text: " + trimmedText);
+  }
+  
+  let text = "   Example text   ";
+  removeUnnecessarySpaces(text);
+  
+  // Trimmed Text: Example text
