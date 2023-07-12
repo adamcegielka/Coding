8a4db50753852629633055b2bc0ef8caf92285cb
Adam <adamcegielka@tlen.pl>
2023-07-12 14:28:03 +0200

    feat: add function in `length.js` file

--- JavaScript-My-Lerning/basics/String Methods/length.js
+++ JavaScript-My-Lerning/basics/String Methods/length.js
@@ -5,3 +5,15 @@ console.log(someText.length); // 26
 
 let lengthText = someText.length;
 console.log(lengthText); // 26
+
+// --------------------------------------
+
+function calculateLength(text) {
+    let long = text.length;
+    console.log('Text length: ' + long);
+}
+
+let sameText = 'Lorem Ipsum is simply dummy text of the printing, is typesetting industry.';
+calculateLength(sameText);
+
+// Text length: 74
