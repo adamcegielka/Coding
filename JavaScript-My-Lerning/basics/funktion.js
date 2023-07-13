e2cbd93333670eebc593ee0b0a066f56fce0ec4b
Adam <adamcegielka@tlen.pl>
2023-07-13 16:34:26 +0200

    fix(style): improve code readability

--- JavaScript-My-Lerning/basics/funktion.js
+++ JavaScript-My-Lerning/basics/funktion.js
@@ -7,7 +7,7 @@ function nameFunction(a, b) {
 let values = nameFunction(4, 7);
 console.log('Multiplication result: ' + values);
 
-console.log('---- NEXT ----');
+// --------------------------------------
 
 function add(num1, num2) {
   let result = num1 + num2;
@@ -15,7 +15,7 @@ function add(num1, num2) {
 }
 add(8, 9);
 
-console.log('---- NEXT ----');
+// --------------------------------------
 
 function returnAdd(num1, num2) {
   let result = num1 + num2;
@@ -24,13 +24,13 @@ function returnAdd(num1, num2) {
 const myResult = returnAdd(18, 9);
 console.info(myResult);
 
-console.log('---- NEXT ----');
+// --------------------------------------
 
 const x = (num1, num2) => num1 + num2;
 const arrowResult = x(28, 9);
 console.warn(arrowResult);
 
-console.log('---- NEXT ----');
+// --------------------------------------
 
 function mathematicalOperation(a, b, c, d) {
   let result = (a * b) / c - d;
@@ -42,7 +42,7 @@ console.log('Result of a mathematical operation: ' + resultOfAction); // 8
 let resultOfAction2 = mathematicalOperation(12, 22, 8, 10);
 console.log(resultOfAction2); // 23
 
-console.log('---- NEXT ----');
+// --------------------------------------
 
 function getSomeNumber() {
   return 666;
