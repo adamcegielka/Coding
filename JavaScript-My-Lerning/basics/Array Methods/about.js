ee621740dbc048693d41a00383c5ae4bad27f905
Adam <adamcegielka@tlen.pl>
2023-07-14 09:54:10 +0200

    feat: add `about.js` file in Array Methods

---
+++ JavaScript-My-Lerning/basics/Array Methods/about.js
@@ -0,0 +1,15 @@
+// Array Methods
+// https://www.w3schools.com/js/js_array_methods.asp
+
+length
+delete(x)
+concat()
+flat()
+join()
+pop()
+push()
+shift()
+slice()
+splice()
+toString()
+unshift()
