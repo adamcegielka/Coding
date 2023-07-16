80312b04b0d1793ed6f472df843a69f386e440aa
Adam <adamcegielka@tlen.pl>
2023-07-16 14:02:02 +0200

    feat: add function in `pop().js` file

--- JavaScript-My-Lerning/basics/Array Methods/pop().js
+++ JavaScript-My-Lerning/basics/Array Methods/pop().js
@@ -3,3 +3,16 @@
 const names = ['Adam', 'Ewa', 'John', 'Ola'];
 names.pop();
 console.log(names); // (3) ['Adam', 'Ewa', 'John']
+
+// --------------------------------------
+
+function removeLastElement(arr, element) {
+    arr.pop(element);
+
+    return arr;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6,];
+let lastElement = 7;
+let result = removeLastElement(numbers, lastElement);
+console.log(result); // (5) [1, 2, 3, 4, 5]
