004b44b7b216e36a76801710f1cf82a27a753897
Adam <adamcegielka@tlen.pl>
2023-07-16 13:58:56 +0200

    feat: add function in `push().js` file

--- JavaScript-My-Lerning/basics/Array Methods/push().js
+++ JavaScript-My-Lerning/basics/Array Methods/push().js
@@ -3,3 +3,16 @@
 const names = ['Adam', 'Ewa', 'John', 'Ola'];
 names.push('Mark');
 console.log(names); // (5) ['Adam', 'Ewa', 'John', 'Ola', 'Mark']
+
+// --------------------------------------
+
+function addElement(arr, element) {
+    arr.push(element);
+
+    return arr;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6,];
+let newElement = 7;
+let result = addElement(numbers, newElement);
+console.log(result); // (7) [1, 2, 3, 4, 5, 6, 7]
