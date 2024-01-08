f76ca82e96140897f2ed925a5c03573f89315347
Adam Cegiełka <adamcegielka@tlen.pl>
2024-01-08 14:34:13 +0100

    feat: add new task `27`

---
+++ JavaScript-My-Lerning/tasks/task27_type-conversion.js
@@ -0,0 +1,20 @@
+// Type Conversion - challenge
+
+console.log(typeof + '12', ' - ', '12'); // number  -  12
+
+console.log(typeof - '9', ' - ', '9'); // number  -  9
+
+console.log(typeof String(321), ' - ', String(321)); // string  -  321
+
+console.log(typeof parseInt('52'), ' - ', parseInt('52')); // number  -  52
+
+console.log(typeof parseFloat('97.43'), ' - ', parseFloat('97.43')); // number  -  97.43
+
+console.log(typeof Math.round(101.6), ' - ', Math.round(101.6)); // number  -  102
+console.log(typeof Math.round(101.4), ' - ', Math.round(101.4)); // number  -  101
+
+console.log(typeof Math.floor(29.678), ' - ', Math.floor(29.678)); // number  -  29
+
+console.log(typeof Math.ceil(3.33), ' - ', Math.ceil(3.33)); // number  -  4
+
+console.log(typeof Math.trunc(77.77), ' - ', Math.trunc(77.77)); // number  -  77
